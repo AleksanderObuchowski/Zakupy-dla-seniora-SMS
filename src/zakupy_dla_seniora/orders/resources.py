88fcf3553f1b7b9024fd4da3d692d1b551b6ac3a
Bartosz Rydziński <rydzinski.bartosz.1998@gmail.com>
2020-03-19 12:38:43 +0000

    Update resources.py

--- src/zakupy_dla_seniora/orders/resources.py
+++ src/zakupy_dla_seniora/orders/resources.py
@@ -3,8 +3,8 @@ from zakupy_dla_seniora.orders.models import Order
 
 
 register_parser = reqparse.RequestParser()
-register_parser.add_argument('id_user', help='This field cannot be blank', required=True)
-register_parser.add_argument('id_message', help='This field cannot be blank', required=True)
+register_parser.add_argument('user_id', help='This field cannot be blank', required=True)
+register_parser.add_argument('message_id', help='This field cannot be blank', required=True)
 register_parser.add_argument('order_status', help='This field cannot be blank', required=True)
 register_parser.add_argument('order_date', help='This field cannot be blank', required=True)
 
@@ -14,8 +14,8 @@ class OrderCreation(Resource):
     def post(self):
         data = register_parser.parse_args()
         new_order = Orders(
-            id_user=data['id_user'],
-            id_message=data['id_message'],
+            user_id=data['user_id'],
+            message_id=data['message_id'],
             order_status=data['order_status'],
             order_date=data['order_date'],
         )
