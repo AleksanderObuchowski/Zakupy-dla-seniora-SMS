d98bd4cc40680e8762a1d01342a144c7f5bcac82
Avienir <obuchowskialeksander@gmail.com>
2020-03-21 02:03:12 +0100

    no

--- src/zakupy_dla_seniora/main/routes.py
+++ src/zakupy_dla_seniora/main/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
-
+import requests
 
 @main.route('/')
 def home():
     return "<h1>Hello</h1>"
+@main.route('/board_test')
+def board():
+    r = requests.get('http://127.0.0.1:5000/board')
+    print(r)
+    return render_template('board.html', data = r.json())
